6b8fb6e7a5edcf1df0f58b793662d4d4b265991c
hiroshi <hiroshi@websoft.jp>
2023-03-07 15:42:11 +0900

    Create pl from a template

---
+++ pl/pl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  pl
+//
+//  Created by Hiroshi Tadokoro on 2023/03/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
