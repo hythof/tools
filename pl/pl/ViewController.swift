8c6bce7f6a42787b8f7971252b4c022e7358d8b4
hiroshi <hiroshi@websoft.jp>
2023-03-07 15:58:45 +0900

    Show local html

--- pl/pl/ViewController.swift
+++ pl/pl/ViewController.swift
@@ -6,21 +6,28 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let file = Bundle.main.url(forResource: "index", withExtension: "html")!
+        let dir = file.deletingLastPathComponent()
+        webView.loadFileURL(file, allowingReadAccessTo: dir)
     }
+}
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+extension ViewController: WKUIDelegate {
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+       print("*** didFail: \(error)")
     }
-
-
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print("*** didFailProvisionalNavigation: \(error)")
+    }
 }
 
